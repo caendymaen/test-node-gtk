89c437cdea1af3a997dcfa9c3c8c29c4fb42a922
caendymaen <grill.andreas@aon.at>
2024-05-18 09:31:11 +0200

    lot of nix tests

--- nix/default.nix
+++ nix/default.nix
@@ -1,42 +1,97 @@
-{ lib
-, stdenv
-, buildNpmPackage
-, fetchFromGitLab,
-nodejs
-, nodePackages
-, gobject-introspection
-, gtk4
-, wrapGAppsHook
-, gtk4-layer-shell
+{ 
+  lib,
+  stdenv,
+  buildNpmPackage,
+  nodejs,
+  gobject-introspection,
+  gobject-introspection-unwrapped,
+  gtk3,
+  gtk4,
+  wrapGAppsHook,
+  gtk4-layer-shell,
+  autoPatchelfHook,
+  glib,
+  glibc,
+  python3,
+  cairo,
+  gjs
 }:
 
 stdenv.mkDerivation rec {
   pname = "node-gtk-test";
   version = "1";
 
-  src = buildNpmPackage {
+  src = buildNpmPackage rec {
     name = pname;
-    src = lib.cleanSource ../.;
-    #src = ../.;
+    #src = lib.cleanSource ../.;
+    src = ../.;#
 
     dontBuild = true;
-
-    npmDepsHash = "sha256-YyAqIKsJAZwh/Nkvc6733IPvqfIHL/XQ4voY0IFcjZU=";
-
-    installPhase = ''
-      mkdir -p $out; cp -r * $out;
-    '';
-  };
+    dontWrapGApps = true;
 
   nativeBuildInputs = [
     wrapGAppsHook
+    autoPatchelfHook
+    gjs
+    glib
+    glib.dev
     gobject-introspection
-    nodejs
+    gobject-introspection-unwrapped
   ];
 
   buildInputs = [
+    gtk3
     gtk4
     gtk4-layer-shell
+    nodejs
+    glib
+    glib.dev
+    #glib
+    python3
+    cairo
+  ];
+
+    npmDepsHash = "sha256-YyAqIKsJAZwh/Nkvc6733IPvqfIHL/XQ4voY0IFcjZU=";
+  installPhase = ''
+      mkdir -p $out
+      cp -r ./ $out;
+    '';
+
+    #outputs = [ "out" ];
+  };
+
+  #src = ../.;
+ installPhase = ''
+    mkdir -p $out
+  #  mkdir -p $out/node_modules
+    cp -r ./ $out 
+  #  cp -r node_modules $out/node_modules
+  '';
+    dontWrapGApps = true;
+
+  nativeBuildInputs = [
+    wrapGAppsHook
+    autoPatchelfHook
+    gobject-introspection
+    gobject-introspection-unwrapped
+    glib
+    glib.dev
+    gjs
+  ];
+
+  buildInputs = [
+    gtk3
+    gtk4
+    gtk4-layer-shell
+    nodejs
+    glib.dev
+    #glib
+    python3
+    cairo
+  ];
+
+  propagatedBuildInputs = [
+    #glib
   ];
 
   outputs = [ "out" ];
